a840cd644de986a68d15e8ed7c92446cb97a8d19
itplamen <itplamen@gmail.com>
2024-07-12 17:21:20 +0300

    Added cache data provider

---
+++ ReactFootball.Backend/Services/ReactFootball.Services.DataProviders/CacheDataProvider.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+
+using ReactFootball.Services.Cache.Contracts;
+using ReactFootball.Services.DataProviders.Contracts;
+using ReactFootball.Services.Models.Contracts;
+
+namespace ReactFootball.Services.DataProviders
+{
+    public class CacheDataProvider<TRequest, TResponse> : IDataProvider<TRequest, TResponse>
+        where TRequest : IRequest
+        where TResponse : class
+    {
+        private readonly ICacheService<TResponse> cacheService;
+        private readonly IDataProvider<TRequest, TResponse> decorated;
+
+        public CacheDataProvider(ICacheService<TResponse> cacheService, IDataProvider<TRequest, TResponse> decorated)
+        {
+            this.cacheService = cacheService;
+            this.decorated = decorated;
+        }
+
+        public async Task<TResponse> GetData(TRequest request)
+        {
+            var cacheData = await cacheService.Get(request.CacheKey);
+
+            if (cacheData == null)
+            {
+                var response = await decorated.GetData(request);
+                await cacheService.Set(request.CacheKey, response);
+
+                return response;
+            }
+
+            return cacheData;
+        }
+    }
+}
