e8e8553a4e7b574760a179d93df1fff27b256b05
itplamen <itplamen@gmail.com>
2024-07-12 14:39:16 +0300

    Created base data provider contract and query string builder utils

---
+++ ReactFootball.Backend/Services/ReactFootball.Services.DataProviders/Contracts/IDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+using ReactFootball.Services.Models.Contracts;
+
+namespace ReactFootball.Services.DataProviders.Contracts
+{
+    public interface IDataProvider<TRequest, TResponse>
+        where TRequest : IRequest
+        where TResponse : class
+    {
+        Task<TResponse> GetData(TRequest request);
+    }
+}
---
+++ ReactFootball.Backend/Services/ReactFootball.Services.DataProviders/Utilities/QueryStringBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using ReactFootball.Services.Models.Contracts;
+
+namespace ReactFootball.Services.DataProviders.Utilities
+{
+    public static class QueryStringBuilder
+    {
+        public static IDictionary<string, string> Build(IRequest request)
+        {
+            var queryString = new Dictionary<string, string>();
+            var properties = request.GetType().GetProperties();
+
+            foreach (var prop in properties)
+            {
+                if (prop.CustomAttributes.Any())
+                {
+                    string name = prop.CustomAttributes.First().ConstructorArguments.First().Value?.ToString();
+                    string value = prop.GetValue(request)?.ToString();
+
+                    if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(value))
+                    {
+                        if (prop.PropertyType != typeof(int) ||
+                            (int.TryParse(value, out int result) &&
+                            result != 0))
+                        {
+                            queryString.Add(name, value);
+                        }
+                    }
+                }
+            }
+
+            return queryString;
+        }
+    }
+}
