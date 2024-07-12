ae7a82e309668b0dc2fcd45dd9de3c54b88b4234
itplamen <itplamen@gmail.com>
2024-07-12 14:43:05 +0300

    Created in-memory cache service

---
+++ ReactFootball.Backend/Services/ReactFootball.Services.Cache/Contracts/ICacheService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ReactFootball.Services.Cache.Contracts
+{
+    public interface ICacheService<TValue>
+        where TValue : class
+    {
+        Task Set(string key, TValue value);
+
+        Task<TValue> Get(string key);
+    }
+}
---
+++ ReactFootball.Backend/Services/ReactFootball.Services.Cache/InMemoryCacheService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Caching.Memory;
+
+using ReactFootball.Services.Cache.Contracts;
+
+namespace ReactFootball.Services.Cache
+{
+    public class InMemoryCacheService<TValue> : ICacheService<TValue>
+        where TValue : class
+    {
+        private readonly IMemoryCache memoryCache;
+        private readonly MemoryCacheEntryOptions options;
+
+        public InMemoryCacheService(IMemoryCache memoryCache, int expiration)
+        {
+            this.memoryCache = memoryCache;
+            this.options = new MemoryCacheEntryOptions();
+            options.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(expiration);
+        }
+
+        public Task Set(string key, TValue value)
+        {
+            memoryCache.Set(key, value, options);
+
+            return Task.CompletedTask;
+        }
+
+        public Task<TValue> Get(string key)
+        {
+            var cacheData = memoryCache.Get<TValue>(key);
+
+            return Task.FromResult(cacheData);
+        }
+    }
+}
