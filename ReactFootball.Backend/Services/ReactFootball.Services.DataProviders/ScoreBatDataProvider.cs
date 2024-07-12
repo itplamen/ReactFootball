840bc35e3ebfa1084e68c905f65177fad9b096eb
itplamen <itplamen@gmail.com>
2024-07-12 14:39:31 +0300

    Added ScoreBat data provider

---
+++ ReactFootball.Backend/Services/ReactFootball.Services.DataProviders/ScoreBatDataProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json;
+
+using ReactFootball.Services.DataProviders.Contracts;
+using ReactFootball.Services.DataProviders.Utilities;
+using ReactFootball.Services.Models.ScoreBat;
+
+namespace ReactFootball.Services.DataProviders
+{
+    public class ScoreBatDataProvider : IDataProvider<ScoreBatRequestModel, ScoreBatResponseModel>
+    {
+        private readonly HttpClient httpClient;
+
+        public ScoreBatDataProvider(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<ScoreBatResponseModel> GetData(ScoreBatRequestModel request)
+        {
+            var queryString = QueryStringBuilder.Build(request);
+            string url = QueryHelpers.AddQueryString(request.Url, queryString);
+
+            var response = await httpClient.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ScoreBatResponseModel>(content);
+
+            return result;
+        }
+    }
+}
